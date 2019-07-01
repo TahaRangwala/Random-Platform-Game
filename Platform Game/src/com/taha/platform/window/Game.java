5ab1e0ebeb831aed71edd9a74d3cb91f621989b9
Taha Rangwala <tahamrangwala@gmail.com>
2019-07-01 11:50:00 -0400

    added game loop

--- Platform Game/src/com/taha/platform/window/Game.java
+++ Platform Game/src/com/taha/platform/window/Game.java
@@ -1,6 +1,9 @@
 package com.taha.platform.window;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
 	
@@ -16,7 +19,53 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void run() {
-		System.out.println("Thread has begun");
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+				   
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		//Draw stuff in game here
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main (String [] args) {
--- Platform Game/src/com/taha/platform/window/Window.java
+++ Platform Game/src/com/taha/platform/window/Window.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 public class Window {
 	
 	public Window(int w, int h, String title, Game game) {
-		
 		game.setPreferredSize(new Dimension(w,h));
 		game.setMaximumSize(new Dimension(w,h));
 		game.setMinimumSize(new Dimension(w,h));
