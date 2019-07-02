9968b1664838b588edb9dfb77cdb537acfee288b
Taha Rangwala <tahamrangwala@gmail.com>
2019-07-02 10:56:16 -0400

    collision boxes

--- Platform Game/src/com/taha/platform/objects/Player.java
+++ Platform Game/src/com/taha/platform/objects/Player.java
@@ -2,6 +2,7 @@ package com.taha.platform.objects;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 
@@ -21,7 +22,7 @@ public class Player extends GameObject{
 
 	public void tick(LinkedList<GameObject> object) {
 		x += velocityX;
-		y += velocityY;
+		//y += velocityY;
 		
 		if(falling || jumping) {
 			velocityY += gravity;
@@ -35,10 +36,31 @@ public class Player extends GameObject{
 	public void render(Graphics g) {
 		g.setColor(Color.blue);
 		g.fillRect((int)x, (int)y, (int)width, (int)height);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g.setColor(Color.red);
+		g2d.draw(getBounds());
+		g2d.draw(getBoundsRight());
+		g2d.draw(getBoundsLeft());
+		g2d.draw(getBoundsTop());
 	}
 
 	public Rectangle getBounds() {
-		return new Rectangle((int)x, (int)y, (int)width, (int)height);
+		return new Rectangle((int) ((int)x+(width/2) - ((width/2)/2)), (int) ((int)y+(height/2)), (int)width/2, (int)height/2);
 	}
+	
+	public Rectangle getBoundsTop() {
+		return new Rectangle((int) ((int)x+(width/2)-(width/2/2)), (int)y, (int)width/2, (int)height/2);
+	}
+	
+	public Rectangle getBoundsRight() {
+		return new Rectangle((int) ((int)x+width-5), (int)y+5, (int)5, (int)height-10);
+	}
+	
+	public Rectangle getBoundsLeft() {
+		return new Rectangle((int)x, (int)y+5, (int)5, (int)height-10);
+	}
+	
+	
 	
 }
